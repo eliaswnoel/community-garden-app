a8567255e7f9e222a8099d6fbd68cffaa53990a8
eliaswnoel <eliaswnoel@gmail.com>
2024-03-31 13:59:54 -0400

    packages

---
+++ models/garden.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+
+const gardenSchema = new Schema ({
+  name: {type:String, required: true},
+  address: {type: String, required: true},
+  startDate: {type: Date, required: true}  
+}, {
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('Garden', gardenSchema);
